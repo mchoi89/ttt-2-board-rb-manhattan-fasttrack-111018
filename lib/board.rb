d8ddb82cd0112f9922c748085074ca571bdd937c
mchoi89 <mchoi89@gmail.com>
2018-11-10 18:51:53 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,4 @@
-# Define the variable board below.
-def (board)
-  board = [" "," "," "," "," "," "," "," "," "]
-end
-f display_board(board)
+display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
