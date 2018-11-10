933494a3225e0f8541783071838a670942b6daac
mchoi89 <mchoi89@gmail.com>
2018-11-10 18:45:34 +0000

    automatically backed up by learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,4 @@
 # Define the variable board below.
 def board
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
-  puts "-----------"
-  puts "   |   |   "
+  board = [" "," "," "," "," "," "," "," "," "]
 end
